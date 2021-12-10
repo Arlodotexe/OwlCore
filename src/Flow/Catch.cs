7f30719c8714e00baab222c99f2ecb34305bdcad
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-12-10 20:50:08 +0000

    Renamed Flow.TryOrSkip to Flow.Catch. Flow.Catch now properly returns default values when an exception is caught. Flow.Catch now has an overload that catches all exception with no return value.

--- src/Flow/Catch.cs
+++ src/Flow/Catch.cs
@@ -10,10 +10,25 @@ namespace OwlCore
     public static partial class Flow
     {
         /// <summary>
-        /// Syntactic sugar for catching any exception that may occur in a <see cref="Task"/>.
+        /// Syntactic sugar for catching any exception in a callback.
         /// </summary>
         /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
-        public static void TryOrSkip<TException>(Action action)
+        public static void Catch(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch
+            {
+            }
+        }
+
+        /// <summary>
+        /// Syntactic sugar for catching a specific exception in a callback.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static void Catch<TException>(Action action)
             where TException : Exception
         {
             try
@@ -22,42 +37,39 @@ namespace OwlCore
             }
             catch (TException)
             {
-                // ignored
             }
         }
 
         /// <summary>
-        /// Syntactic sugar for catching a specific exception that may occur in a <see cref="Task"/>.
+        /// Syntactic sugar for catching a specific exception in a callback with a return value.
         /// </summary>
         /// <returns>A <see cref="Task"/> that represents the asynchronous operation. Default (nullable) if the task fails.</returns>
-        public static TResult TryOrSkip<TException, TResult>(Func<TResult> task)
+        public static TResult? Catch<TException, TResult>(Func<TResult> func)
             where TException : Exception
-            where TResult : class
         {
             try
             {
-                return task();
+                return func();
             }
             catch (TException)
             {
-                return default!;
+                return default;
             }
         }
 
         /// <summary>
-        /// Syntactic sugar for catching any exception that may occur in a <see cref="Task"/>.
+        /// Syntactic sugar for catching an exception in a callback with a return value.
         /// </summary>
         /// <returns>A <see cref="Task"/> that represents the asynchronous operation. Default (nullable) if the task fails.</returns>
-        public static TResult TryOrSkip<TResult>(Func<TResult> task)
+        public static TResult? Catch<TResult>(Func<TResult> func)
         {
             try
             {
-                return task();
+                return func();
             }
             catch
             {
-                // todo fix null silencing. Can't use a class constraint.
-                return default!;
+                return default;
             }
         }
     }
