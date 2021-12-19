269a74849bae14a32e35a33c54647f95aab91430
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-12-19 01:09:46 +0000

    Remove problematic Flow.Catch overloads and added tests

--- src/Flow/Catch.cs
+++ src/Flow/Catch.cs
@@ -25,22 +25,6 @@ namespace OwlCore
         }
 
         /// <summary>
-        /// Syntactic sugar for catching any exception in a callback with a return value.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation. Default (nullable) if the task fails.</returns>
-        public static TResult? Catch<TResult>(Func<TResult> func)
-        {
-            try
-            {
-                return func();
-            }
-            catch
-            {
-                return default;
-            }
-        }
-
-        /// <summary>
         /// Syntactic sugar for catching a specific exception in a callback.
         /// </summary>
         /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
@@ -57,74 +41,35 @@ namespace OwlCore
         }
 
         /// <summary>
-        /// Syntactic sugar for catching a specific exception in a callback.
+        /// Syntactic sugar for catching any exception in a callback with a return value.
         /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
-        public static void Catch<TException, TException2>(Action action)
-            where TException : Exception
-            where TException2 : Exception
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation. Default (nullable) if the task fails.</returns>
+        public static TResult? Catch<TResult>(Func<TResult> func)
         {
-            try
+            // There's no "where TResult : not Exception"
+            // so this overload picks up Catch{TException}(Action), so if you write:
+            // Flow.Catch<NotImplementedException>(() => throw new InvalidOperationException());
+            // It uses this overload and doesn't catch without this manual check.
+            if (typeof(Exception).IsAssignableFrom(typeof(TResult)))
             {
-                action();
-            }
-            catch (TException)
-            {
-            }
-            catch (TException2)
-            {
-            }
-        }
+                try
+                {
+                    return func();
+                }
+                catch (Exception exc) when (exc is TResult)
+                {
 
-        /// <summary>
-        /// Syntactic sugar for catching a specific exception in a callback.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
-        public static void Catch<TException, TException2, TException3>(Action action)
-            where TException : Exception
-            where TException2 : Exception
-            where TException3 : Exception
-        {
-            try
-            {
-                action();
+                return default;
+                }
             }
-            catch (TException)
-            {
-            }
-            catch (TException2)
-            {
-            }
-            catch (TException3)
-            {
-            }
-        }
 
-        /// <summary>
-        /// Syntactic sugar for catching a specific exception in a callback.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
-        public static void Catch<TException, TException2, TException3, TException4>(Action action)
-            where TException : Exception
-            where TException2 : Exception
-            where TException3 : Exception
-            where TException4 : Exception
-        {
             try
             {
-                action();
+                return func();
             }
-            catch (TException)
+            catch
             {
-            }
-            catch (TException2)
-            {
-            }
-            catch (TException3)
-            {
-            }
-            catch (TException4)
-            {
+                return default;
             }
         }
 
@@ -140,87 +85,6 @@ namespace OwlCore
                 return func();
             }
             catch (TException)
-            {
-                return default;
-            }
-        }
-
-        /// <summary>
-        /// Syntactic sugar for catching a specific exception in a callback with a return value.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation. Default (nullable) if the task fails.</returns>
-        public static TResult? Catch<TResult, TException, TException2>(Func<TResult> func)
-            where TException : Exception
-            where TException2 : Exception
-        {
-            try
-            {
-                return func();
-            }
-            catch (TException)
-            {
-                return default;
-            }
-            catch (TException2)
-            {
-                return default;
-            }
-        }
-
-        /// <summary>
-        /// Syntactic sugar for catching a specific exception in a callback with a return value.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation. Default (nullable) if the task fails.</returns>
-        public static TResult? Catch<TResult, TException, TException2, TException3>(Func<TResult> func)
-            where TException : Exception
-            where TException2 : Exception
-            where TException3 : Exception
-        {
-            try
-            {
-                return func();
-            }
-            catch (TException)
-            {
-                return default;
-            }
-            catch (TException2)
-            {
-                return default;
-            }
-            catch (TException3)
-            {
-                return default;
-            }
-        }
-
-        /// <summary>
-        /// Syntactic sugar for catching a specific exception in a callback with a return value.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation. Default (nullable) if the task fails.</returns>
-        public static TResult? Catch<TResult, TException, TException2, TException3, TException4>(Func<TResult> func)
-            where TException : Exception
-            where TException2 : Exception
-            where TException3 : Exception
-            where TException4 : Exception
-        {
-            try
-            {
-                return func();
-            }
-            catch (TException)
-            {
-                return default;
-            }
-            catch (TException2)
-            {
-                return default;
-            }
-            catch (TException3)
-            {
-                return default;
-            }
-            catch (TException4)
             {
                 return default;
             }
---
+++ tests/Flow/Catch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OwlCore.Tests.Flow
+{
+    [TestClass]
+    public class CatchTests
+    {
+        [TestMethod, Timeout(1000)]
+        public void CatchSimple()
+        {
+            OwlCore.Flow.Catch(() => throw new NotImplementedException());
+        }
+
+        [TestMethod, Timeout(1000)]
+        public void CatchWithResult()
+        {
+            var val = 5;
+
+            var returnedValue = OwlCore.Flow.Catch(() => val);
+            var defaultValue = OwlCore.Flow.Catch<int>(() => throw new NotImplementedException());
+
+            // To properly test, these must not be the same.
+            Assert.AreNotEqual(val, defaultValue);
+
+            Assert.AreEqual(val, returnedValue);
+            Assert.AreEqual(default, defaultValue);
+        }
+
+        [TestMethod, Timeout(1000)]
+        public void CatchSpecificException()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                OwlCore.Flow.Catch<NotImplementedException>(() => { throw new InvalidOperationException(); });
+            });
+
+            OwlCore.Flow.Catch<NotImplementedException>(() => throw new NotImplementedException());
+        }
+
+        [TestMethod, Timeout(1000)]
+        public void CatchSpecificExceptionWithResult()
+        {
+            var val = 5;
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                _ = OwlCore.Flow.Catch<int, NotImplementedException>(() => throw new InvalidOperationException());
+            });
+
+            var returnedValue = OwlCore.Flow.Catch<int, NotImplementedException>(() => val);
+            var defaultValue = OwlCore.Flow.Catch<int, NotImplementedException>(() => throw new NotImplementedException());
+
+            // To properly test, these must not be the same.
+            Assert.AreNotEqual(val, defaultValue);
+
+            Assert.AreEqual(val, returnedValue);
+            Assert.AreEqual(default, defaultValue);
+        }
+    }
+}
