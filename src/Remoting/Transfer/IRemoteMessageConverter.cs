fd919acb5b95570ad72511e582636b320ca36b56
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-03-02 10:11:28 -0600

    Switched from custom methods to IAsyncSerializer{T, T2}

--- src/Remoting/Transfer/IRemoteMessageConverter.cs
+++ src/Remoting/Transfer/IRemoteMessageConverter.cs
@@ -1,6 +1,7 @@
 ﻿using OwlCore.Remoting.Transfer.MessageConverters;
 using System.Threading;
 using System.Threading.Tasks;
+using OwlCore.Services;
 
 namespace OwlCore.Remoting.Transfer
 {
@@ -14,22 +15,7 @@ namespace OwlCore.Remoting.Transfer
     /// When crafting your own <see cref="IRemoteMessageConverter"/>, keep in mind that unless you're extra careful to remote only primitive types, object instances being serialized may need to handle circular references.
     /// </para>
     /// </remarks>
-    public interface IRemoteMessageConverter
+    public interface IRemoteMessageConverter : IAsyncSerializer<byte[], IRemoteMessage>
     {
-        /// <summary>
-        /// Converts the given <paramref name="message"/> to a byte array that is safe for generic data transfer.
-        /// </summary>
-        /// <param name="message">The message being sent.</param>
-        /// <param name="cancellationToken">A cancellation token that can be used to cancel the ongoing task.</param>
-        /// <returns></returns>
-        public Task<byte[]> SerializeAsync(IRemoteMessage message, CancellationToken? cancellationToken = null);
-
-        /// <summary>
-        /// Converts the given byte array back to a valid <see cref="IRemoteMessage"/>.
-        /// </summary>
-        /// <param name="bytes">The byte array to deserialize.</param>
-        /// <param name="cancellationToken">A cancellation token that can be used to cancel the ongoing task.</param>
-        /// <returns></returns>
-        public Task<IRemoteMessage> DeserializeAsync(byte[] bytes, CancellationToken? cancellationToken = null);
     }
 }
