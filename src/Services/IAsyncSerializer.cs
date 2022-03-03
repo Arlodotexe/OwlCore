253357825a9fb7c8521767c0f6518464796acac9
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-03-02 23:41:21 -0600

    Add non-generic overloads with Type param

--- src/Services/IAsyncSerializer.cs
+++ src/Services/IAsyncSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OwlCore.Services
@@ -17,6 +18,15 @@ namespace OwlCore.Services
         /// <typeparam name="T">The type of the object being serialized.</typeparam>
         /// <returns>A serialized instance of <paramref name="data"/>.</returns>
         public Task<TSerialized> SerializeAsync<T>(T data, CancellationToken? cancellationToken = null);
+        
+        /// <summary>
+        /// Serializes the provided <paramref cref="data"/> into <typeparam name="TSerialized"/>.
+        /// </summary>
+        /// <param name="data">The object instance to serialize.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the ongoing task.</param>
+        /// <param name="inputType">The type of the <paramref name="data" /> object being serialized.</param>
+        /// <returns>A serialized instance of <paramref name="data"/>.</returns>
+        public Task<TSerialized> SerializeAsync(Type inputType, object data, CancellationToken? cancellationToken = null);
 
         /// <summary>
         /// Deserializes the provided <paramref name="serialized"/> data into the given type.
@@ -26,6 +36,15 @@ namespace OwlCore.Services
         /// <typeparam name="TResult">The type to deserialize to.</typeparam>
         /// <returns>A deserialized instance of the provided serialized data.</returns>
         public Task<TResult> DeserializeAsync<TResult>(TSerialized serialized, CancellationToken? cancellationToken = null);
+
+        /// <summary>
+        /// Deserializes the provided <paramref name="serialized"/> data into the given type.
+        /// </summary>
+        /// <param name="serialized">A serialized instance to deserialize.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the ongoing task.</param>
+        /// <param name="returnType">The return type of the object being deserialized.</param>
+        /// <returns>A deserialized instance of the provided serialized data.</returns>
+        public Task<object> DeserializeAsync(Type returnType, TSerialized serialized, CancellationToken? cancellationToken = null);
     }
     
     /// <summary>
--- src/Services/ISerializer.cs
+++ src/Services/ISerializer.cs
@@ -1,4 +1,6 @@
-﻿namespace OwlCore.Services
+﻿using System;
+
+namespace OwlCore.Services
 {
     /// <summary>
     /// An interface that allows serializing data to and from <typeparamref name="TSerialized"/> synchronously.
@@ -13,6 +15,14 @@
         /// <typeparam name="T">The type of the object being serialized.</typeparam>
         /// <returns>A serialized instance of <paramref name="data"/>.</returns>
         public TSerialized Serialize<T>(T data);
+        
+        /// <summary>
+        /// Serializes the provided <paramref cref="data"/> into <typeparam name="TSerialized"/>.
+        /// </summary>
+        /// <param name="data">The object instance to serialize.</param>
+        /// <param name="type">The type of the object being serialized.</param>
+        /// <returns>A serialized instance of <paramref name="data"/>.</returns>
+        public TSerialized Serialize(Type type, object data);
 
         /// <summary>
         /// Deserializes the provided <paramref name="serialized"/> data into the given type.
@@ -21,6 +31,14 @@
         /// <typeparam name="TResult">The type to deserialize to.</typeparam>
         /// <returns>A deserialized instance of the provided serialized data.</returns>
         public TResult Deserialize<TResult>(TSerialized serialized);
+
+        /// <summary>
+        /// Deserializes the provided <paramref name="serialized"/> data into the given type.
+        /// </summary>
+        /// <param name="serialized">A serialized instance to deserialize.</param>
+        /// <param name="type">The type of the object being serialized.</param>
+        /// <returns>A deserialized instance of the provided serialized data.</returns>
+        public object Deserialize(Type type, TSerialized serialized);
     }
     
     /// <summary>
